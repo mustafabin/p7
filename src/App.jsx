8e7e15203e5166ae52413efb273d9ecc09607cb6
Mustafa <mbin828@gmail.com>
2022-04-12 16:24:10 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,49 @@ import { useRef, useEffect, useState } from "react";
 import axios from "axios";
 function App() {
   const galleryRef = useRef(null);
-  const galleryDisplay = useRef(null);
+  const spanRef = useRef(null);
+  const arrowRef = useRef(null);
   const [movies, setMovies] = useState([]);
   const [randomTen, setRandomTen] = useState([]);
   let slideGallery = (dir) => {
     galleryRef.current.scrollBy(window.innerWidth * dir, 0);
   };
-
-  useEffect(() => {
-    getmovies();
-  }, []);
-  let hide = "hide";
   let getmovies = async () => {
     let res = await axios.get(
       "https://top100-superhero-movie-api.herokuapp.com/api/movies"
     );
-    console.log(res);
-    setMovies(res.data);
-    updateRandom(movies.slice(0, 9));
+
+    await setMovies(res.data);
+    console.log(movies);
+    updateRandom();
   };
+
+  useEffect(() => {
+    getmovies();
+  }, []);
+
   let updateRandom = () => {
-    galleryDisplay.current.classList.remove("hide");
     let random = Math.random() * (90 - 0) + 0;
     setRandomTen(movies.slice(random, random + 10));
+
+    // spanRef.current.classList.remove("hide");
   };
   return (
     <div className="App">
       <div className="options">
         <h1>Movie Finder</h1>
         <p>
-          Random 10 movies:
+          {randomTen.length} 10 movies:
           <button onClick={updateRandom} className="generate-button">
             Generate
           </button>
         </p>
       </div>
-      <span ref={galleryDisplay} className="hide">
+      <span ref={spanRef} className="hide">
         Gallery
       </span>
       <div className="galler-container">
-        <div className="arrows">
+        <div ref={arrowRef} className="arrows">
           <div onClick={() => slideGallery(1)} className="gallery-arrow right">
             {">"}
           </div>
